3b1b2e76de9be469f21f412abec9eaddc72d71cf
Chelios69 <146766552+Chelios69@users.noreply.github.com>
2023-10-07 18:22:38 +0200

    Update full.js

--- full.js
+++ full.js
@@ -1,39 +1,66 @@
 const fs = require("fs");
+const { parseString } = require("xml2js");
+const { Readable } = require("stream");
 
-const jsonPath = "./full/full2.json"
+const xmlPath = "./full.xml"; 
+const jsonPath = "./full/full2.json"; 
 
-try {
-  // Wczytaj zawartość pliku JSON
-  const jsonData = fs.readFileSync(jsonPath, 'utf8');
-  const data = JSON.parse(jsonData);
+function createXmlObjectStream() {
+  const xmlStream = new Readable({ objectMode: true });
 
-  if (data.offers && data.offers.o) {
-    const offers = data.offers.o;
-    if (Array.isArray(offers)) {
-      // Przetwarzaj oferty
-      offers.forEach((offer) => {
-        if (offer.$ && offer.$.price) {
-          const price = parseFloat(offer.$.price);
-          if (!isNaN(price)) {
-            offer.$.isPriceGreaterThanTen = price > 10;
+  try {
+    const xmlData = fs.readFileSync(xmlPath);
+    parseString(xmlData, (err, result) => {
+      if (err) {
+        xmlStream.emit("error", err);
+      } else {
+        if (result && result.offers && result.offers.o) {
+          const products = result.offers.o;
+          if (Array.isArray(products)) {
+            products.forEach((product) => {
+              if (product.$ && product.$.price) {
+                const price = parseFloat(product.$.price);
+                if (!isNaN(price)) {
+                  product.$.isPriceGreaterThanTen = price > 10;
+                }
+              }
+            });
+          } else if (products.$ && products.$.price) {
+            const price = parseFloat(products.$.price);
+            if (!isNaN(price)) {
+              products.$.isPriceGreaterThanTen = price > 10;
+            }
           }
+        } else {
+          xmlStream.emit(
+            "error",
+            new Error("Nieprawidłowa struktura pliku XML.")
+          );
         }
-      });
-    } else if (offers.$ && offers.$.price) {
-      // Przetwarzaj pojedynczą ofertę
-      const price = parseFloat(offers.$.price);
-      if (!isNaN(price)) {
-        offers.$.isPriceGreaterThanTen = price > 10;
-      }
-    }
 
-    // Zapisz zmodyfikowane dane z powrotem do pliku JSON
-    const newData = JSON.stringify(data, null, 2);
-    fs.writeFileSync(jsonPath, newData);
-    console.log('Poprawnie zapisano nowe dane');
-  } else {
-    console.error('Nieprawidłowa struktura pliku JSON.');
+        // Zapisz dane do pliku JSON
+        fs.writeFileSync(jsonPath, JSON.stringify(result, null, 2), "utf8");
+
+        xmlStream.push(null);
+      }
+    });
+  } catch (err) {
+    xmlStream.emit("error", err);
   }
-} catch (error) {
-  console.error('Wystąpił błąd:', error.message);
+
+  return xmlStream;
 }
+
+const objectStream = createXmlObjectStream();
+
+objectStream.on("data", (object) => {
+  console.log("Nowy obiekt XML:", object);
+});
+
+objectStream.on("end", () => {
+  console.log("Strumień zakończony.");
+});
+
+objectStream.on("error", (err) => {
+  console.error("Błąd strumienia:", err);
+});
